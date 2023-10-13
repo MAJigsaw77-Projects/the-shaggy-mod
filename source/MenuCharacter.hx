a4eb8fb6559a32ab92b2e69f0f43f1b60af60e11
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-10-13 10:33:10 +0300

    Update MenuCharacter.hx

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -1,7 +1,6 @@
 package;
 
 import flixel.FlxSprite;
-import flixel.graphics.frames.FlxAtlasFrames;
 
 class MenuCharacter extends FlxSprite
 {
@@ -17,9 +16,10 @@ class MenuCharacter extends FlxSprite
 		changeCharacter(character);
 	}
 
-	override function update(elapsed:Float)
+	override function update(elapsed:Float):Void
 	{
 		ang++;
+
 		switch (character)
 		{
 			case 'wbshaggy':
@@ -27,12 +27,14 @@ class MenuCharacter extends FlxSprite
 			default:
 				y = sY;
 		}
+
 		super.update(elapsed);
 	}
 
 	public function changeCharacter(?character:String = 'bf')
 	{
 		alpha = 1;
+
 		if (character == '')
 			alpha = 0;
 
@@ -40,6 +42,7 @@ class MenuCharacter extends FlxSprite
 			return;
 
 		this.character = character;
+
 		antialiasing = ClientPrefs.globalAntialiasing;
 
 		switch (character)
@@ -48,35 +51,29 @@ class MenuCharacter extends FlxSprite
 				frames = Paths.getSparrowAtlas('menucharacters/Menu_BF');
 				animation.addByPrefix('idle', "M BF Idle", 24);
 				animation.addByPrefix('confirm', 'M bf HEY', 24, false);
-
 			case 'gf':
 				frames = Paths.getSparrowAtlas('menucharacters/Menu_GF');
 				animation.addByPrefix('idle', "M GF Idle", 24);
-
 			case 'shaggy':
 				frames = Paths.getSparrowAtlas('menucharacters/Menu_Shaggy');
 				animation.addByPrefix('idle', "M Shaggy Idle", 24);
-
 			case 'pshaggy':
 				frames = Paths.getSparrowAtlas('menucharacters/Menu_PShaggy');
 				animation.addByPrefix('idle', "M PShaggy Idle", 24);
-
 			case 'rshaggy':
 				frames = Paths.getSparrowAtlas('menucharacters/Menu_RShaggy');
 				animation.addByPrefix('idle', "M RShaggy Idle", 24);
-
 			case 'wbshaggy':
 				frames = Paths.getSparrowAtlas('menucharacters/Menu_WBShaggy');
 				animation.addByPrefix('idle', "M WBShaggy Idle", 24);
-
 			case 'shaggymatt':
 				frames = Paths.getSparrowAtlas('menucharacters/Menu_ShaggyMatt');
 				animation.addByPrefix('idle', "M ShaggyMatt Idle", 24);
-
 			case 'senpai':
 				frames = Paths.getSparrowAtlas('menucharacters/Menu_Senpai');
 				animation.addByPrefix('idle', "M Senpai Idle", 24);
 		}
+
 		animation.play('idle');
 		updateHitbox();
 
@@ -84,24 +81,8 @@ class MenuCharacter extends FlxSprite
 		{
 			case 'bf':
 				offset.set(15, -40);
-
 			case 'gf':
 				offset.set(0, -25);
-
-			case 'spooky':
-				offset.set(0, -80);
-
-			case 'pico':
-				offset.set(0, -120);
-
-			case 'mom':
-				offset.set(0, 10);
-
-			case 'parents':
-				offset.set(110, 10);
-
-			case 'senpai':
-				offset.set(60, -70);
 			case 'pshaggy':
 				offset.set(-10, 20);
 			case 'shaggymatt':
